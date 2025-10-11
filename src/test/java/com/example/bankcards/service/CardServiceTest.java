834f79d2a2df21ff996cde2c36ef7ada548b1289
yanalatysh <yanalatysh1@gmail.com>
2025-10-11 09:50:13 +0300

    fix code, add tests for transfer method

--- src/test/java/com/example/bankcards/service/CardServiceTest.java
+++ src/test/java/com/example/bankcards/service/CardServiceTest.java
@@ -1,21 +1,29 @@
 package com.example.bankcards.service;
 
-import com.example.bankcards.entity.Card;
-import com.example.bankcards.entity.User;
 import com.example.bankcards.BaseMockServiceTest;
-import com.example.bankcards.service.CardService;
+import com.example.bankcards.dto.UserDto;
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.entity.CardStatus;
+import com.example.bankcards.entity.Role;
+import com.example.bankcards.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class CardServiceTest extends BaseMockServiceTest {
     @Autowired
     CardService cardService;
+
+    @MockBean
+    UserService userService;
 
     @Test
     void testGetBalanceFromNotOwnedCard() {
@@ -31,5 +39,189 @@ public class CardServiceTest extends BaseMockServiceTest {
 
         // when and then
         assertThrows(IllegalStateException.class, () -> cardService.getCardBalance(2L, 1L));
+    }
+
+    @Test
+    void testTransferNotAllowedForBlockedCard() {
+        // preconditions
+        var user = new UserDto(1L, "user@email.com", "", "", Role.ROLE_USER);
+
+        var userEntity = new User();
+        userEntity.setId(1L);
+
+        var sourceCard = new Card();
+        sourceCard.setId(1L);
+        sourceCard.setUser(userEntity);
+        sourceCard.setStatus(CardStatus.BLOCKED);
+        sourceCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        var destinationCard = new Card();
+        destinationCard.setId(2L);
+        destinationCard.setUser(userEntity);
+        destinationCard.setStatus(CardStatus.BLOCKED);
+        destinationCard.setBalance(BigDecimal.ZERO);
+        destinationCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        when(userService.getCurrentUser()).thenReturn(user);
+        when(cardRepository.findById(1L)).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(2L)).thenReturn(Optional.of(destinationCard));
+
+        // when and then
+        assertThrows(IllegalArgumentException.class, () -> cardService.transferBetweenCards(1L, 2L, BigDecimal.ONE));
+    }
+
+    @Test
+    void testTransferNotAllowedForExpiredCard() {
+        // preconditions
+        var user = new UserDto(1L, "user@email.com", "", "", Role.ROLE_USER);
+
+        var userEntity = new User();
+        userEntity.setId(1L);
+
+        var sourceCard = new Card();
+        sourceCard.setId(1L);
+        sourceCard.setUser(userEntity);
+        sourceCard.setStatus(CardStatus.ACTIVE);
+        sourceCard.setExpiryDate(LocalDate.now().minusYears(1));
+
+        var destinationCard = new Card();
+        destinationCard.setId(2L);
+        destinationCard.setUser(userEntity);
+        destinationCard.setBalance(BigDecimal.ZERO);
+        destinationCard.setStatus(CardStatus.BLOCKED);
+        destinationCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        when(userService.getCurrentUser()).thenReturn(user);
+        when(cardRepository.findById(1L)).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(2L)).thenReturn(Optional.of(destinationCard));
+
+        // when and then
+        assertThrows(IllegalArgumentException.class, () -> cardService.transferBetweenCards(1L, 2L, BigDecimal.ONE));
+    }
+
+    @Test
+    void testTransferNotAllowedForZeroAmount() {
+        // preconditions
+        var user = new UserDto(1L, "user@email.com", "", "", Role.ROLE_USER);
+
+        var userEntity = new User();
+        userEntity.setId(1L);
+
+        var sourceCard = new Card();
+        sourceCard.setId(1L);
+        sourceCard.setUser(userEntity);
+        sourceCard.setBalance(BigDecimal.ZERO);
+        sourceCard.setStatus(CardStatus.ACTIVE);
+        sourceCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        var destinationCard = new Card();
+        destinationCard.setId(2L);
+        destinationCard.setUser(userEntity);
+        destinationCard.setBalance(BigDecimal.ZERO);
+        destinationCard.setStatus(CardStatus.ACTIVE);
+        destinationCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        when(userService.getCurrentUser()).thenReturn(user);
+        when(cardRepository.findById(1L)).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(2L)).thenReturn(Optional.of(destinationCard));
+
+        // when and then
+        assertThrows(IllegalArgumentException.class, () -> cardService.transferBetweenCards(1L, 2L, BigDecimal.ONE));
+    }
+
+    @Test
+    void testTransferNotAllowedForAmountOverlappingBalance() {
+        // preconditions
+        var user = new UserDto(1L, "user@email.com", "", "", Role.ROLE_USER);
+
+        var userEntity = new User();
+        userEntity.setId(1L);
+
+        var sourceCard = new Card();
+        sourceCard.setId(1L);
+        sourceCard.setUser(userEntity);
+        sourceCard.setBalance(BigDecimal.ONE);
+        sourceCard.setStatus(CardStatus.ACTIVE);
+        sourceCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        var destinationCard = new Card();
+        destinationCard.setId(2L);
+        destinationCard.setUser(userEntity);
+        destinationCard.setBalance(BigDecimal.ZERO);
+        destinationCard.setStatus(CardStatus.ACTIVE);
+        destinationCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        when(userService.getCurrentUser()).thenReturn(user);
+        when(cardRepository.findById(1L)).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(2L)).thenReturn(Optional.of(destinationCard));
+
+        // when and then
+        assertThrows(IllegalArgumentException.class, () -> cardService.transferBetweenCards(1L, 2L, BigDecimal.TEN));
+    }
+
+    @Test
+    void testTransferNotAllowedForNotOwnedCards() {
+        // preconditions
+        var user = new UserDto(1L, "user@email.com", "", "", Role.ROLE_USER);
+
+        var userEntity = new User();
+        userEntity.setId(2L);
+
+        var sourceCard = new Card();
+        sourceCard.setId(1L);
+        sourceCard.setUser(userEntity);
+        sourceCard.setBalance(BigDecimal.ONE);
+        sourceCard.setStatus(CardStatus.ACTIVE);
+        sourceCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        var destinationCard = new Card();
+        destinationCard.setId(2L);
+        destinationCard.setUser(userEntity);
+        destinationCard.setBalance(BigDecimal.ZERO);
+        destinationCard.setStatus(CardStatus.ACTIVE);
+        destinationCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        when(userService.getCurrentUser()).thenReturn(user);
+        when(cardRepository.findById(1L)).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(2L)).thenReturn(Optional.of(destinationCard));
+
+        // when and then
+        assertThrows(IllegalArgumentException.class, () -> cardService.transferBetweenCards(1L, 2L, BigDecimal.ONE));
+    }
+
+    @Test
+    void testTransferSuccessful() {
+        // preconditions
+        var user = new UserDto(1L, "user@email.com", "", "", Role.ROLE_USER);
+
+        var userEntity = new User();
+        userEntity.setId(1L);
+
+        var sourceCard = new Card();
+        sourceCard.setId(1L);
+        sourceCard.setUser(userEntity);
+        sourceCard.setBalance(BigDecimal.ONE);
+        sourceCard.setStatus(CardStatus.ACTIVE);
+        sourceCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        var destinationCard = new Card();
+        destinationCard.setId(2L);
+        destinationCard.setUser(userEntity);
+        destinationCard.setBalance(BigDecimal.ZERO);
+        destinationCard.setStatus(CardStatus.ACTIVE);
+        destinationCard.setExpiryDate(LocalDate.now().plusYears(1));
+
+        when(userService.getCurrentUser()).thenReturn(user);
+        when(cardRepository.findById(1L)).thenReturn(Optional.of(sourceCard));
+        when(cardRepository.findById(2L)).thenReturn(Optional.of(destinationCard));
+        when(cardRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // when
+        cardService.transferBetweenCards(1L, 2L, BigDecimal.ONE);
+
+        // verify
+        verify(cardRepository, atLeastOnce()).save(argThat(card ->
+                card.getId().equals(2L) && card.getBalance().compareTo(BigDecimal.ONE) == 0
+        ));
     }
 }
